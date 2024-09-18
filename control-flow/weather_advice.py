ea9e90131f0243cf31f51b79a860480f48fd205b
Lawal Abdullah Folarin <sirojudeenopeyemi@gmail.com>
2024-09-18 14:36:33 +0100

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,5 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
-if+weather == 'sunny' :
+if +weather == 'sunny' :
     print("Wear a t-shirt and sunglasses.")  
     ('break')
 elif+weather == 'rainy' :
