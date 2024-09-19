ab4f4dc3a8a75de4962d6f53b5e7db5817efa3a9
Lawal Abdullah <sirojudeenopeyemi@gmail.com>
2024-09-19 08:25:08 +0100

    commit my changes

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,1 +1,5 @@
-number = input("enter a number")
+x, y = (1, 10)
+number = int(input("enter a number to see its multiplication table: "))
+print(f"multiplication table for (number):")
+for i in range (x, y+1):
+    print(number * i)
