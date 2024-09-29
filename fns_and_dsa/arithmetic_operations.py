d7247dddc52ec8ef9165da4340ad1c7543b1a70d
Lawal Abdullah Folarin <sirojudeenopeyemi@gmail.com>
2024-09-29 19:50:53 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def main():
+def perform_operation(num1: float, num2: float, operation: str):
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
