166854b41057a978c05d13a048335271d84d3d58
Lawal Abdullah <sirojudeenopeyemi@gmail.com>
2024-09-29 19:56:54 +0100

    Add shopping_list_manager.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,8 @@ def main():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
+    if num1 / num2 == 0:
+        print("main.py script can recognize and display appropriately.")
     result = perform_operation(num1, num2, operation)
     print(f"Result: {result}")
 
