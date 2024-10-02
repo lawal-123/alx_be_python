db7d440ef90503a1df73b35d389c55b56eac55df
Lawal Abdullah <sirojudeenopeyemi@gmail.com>
2024-10-02 17:27:28 +0100

    commit changes made to explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,3 +1,4 @@
+from date time module import datetime, timedelta
 def explore_datetime():
     current_date = ("%Y-%m-%d %H:%M:%S")
-    print(f"current_date_and time: (current_date_and_time)") 
+    print(f"current_date_and time: (current_date_and_time)")
